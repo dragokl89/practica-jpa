0b1a4758aa6096009249dda815c54c22510a11df
dragokl89 <c-adonis@hotmail.com>
2021-02-23 17:02:09 -0500

    inicio

---
+++ src/main/java/com/devpredator/practicajpa/entity/Restaurante.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.devpredator.practicajpa.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author 4PF28LA_2004
+ *
+ */
+@Entity
+@Table(name = "disquera")
+public class Restaurante {
+
+}
